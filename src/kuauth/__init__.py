3d36153dc636e796d00c4de71d28b65322e3ac6a
Yosei Ushida <146376339+youseiushida@users.noreply.github.com>
2026-05-03 18:02:15 +0900

    release: v0.4.0

--- src/kuauth/__init__.py
+++ src/kuauth/__init__.py
@@ -13,7 +13,7 @@ from kuauth.services.kulms import KULMS
 from kuauth.services.mykuline import MyKULINE
 from kuauth.services.panda import PandA
 
-__version__ = "0.3.4"
+__version__ = "0.4.0"
 
 __all__ = [
     "KULASIS",
